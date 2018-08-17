39c0d79e94f796a1c5ab93741482128cb7da4f61
Fabien Lenoir <fabien.lenoir@gmail.com>
2018-08-17 09:58:10 +0200

    translator public

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 const preMiddleware = require('./preMiddlewares');
 const postMiddlewares = require('./postMiddlewares');
+const translatorForLanguages = require('./controler/Translator');
 const { dependencies } = require('./dependencies');
 
 module.exports = {
   alexa: app,
   middlewares: { ...preMiddleware, ...postMiddlewares },
   dependencies,
+  translatorForLanguages,
 };
