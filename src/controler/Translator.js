681f51e770e123ca3f2c83642268303f2c6d0b80
Fabien Lenoir <fabien.lenoir@gmail.com>
2018-08-22 12:31:42 +0200

    random say

--- src/controler/Translator.js
+++ src/controler/Translator.js
@@ -5,9 +5,10 @@ const AmazonSpeech = require('ssml-builder/amazon_speech');
 const R = require('ramda');
 
 const s = () => new AmazonSpeech();
+const r = (...items) => items[Math.floor(Math.random() * items.length)];
 
 const translatorForLanguages = (translations, fallbackLng = 'fr') => {
-  const resources = R.map(translation => translation({ s }), translations);
+  const resources = R.map(translation => translation({ s, r }), translations);
 
   const translator = (request) => {
     const localizationClient = i18n.use(sprintf).init({
