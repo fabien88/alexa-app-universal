b4bd11ea25a3731bb77151b2f11bc01bb8534278
Fabien Lenoir <fabien.lenoir@gmail.com>
2018-08-29 14:28:02 +0200

    user id log

--- src/preMiddlewares.js
+++ src/preMiddlewares.js
@@ -16,6 +16,7 @@ const recoverSessionFromDBMiddleware = ({ database }) => async (request) => {
   const { userId } = request;
   const session = request.getSession();
   let userSession = session.get('USER_SESSION');
+  console.log({ userId });
   console.inspect({ fromSession: userSession });
   // We fetch session from DB only if needed
   if (!userSession) {
